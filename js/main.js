bb792318f3d229cfa93664bbe5d27f7fc7e6b932
Weverton <wever-santos@hotmail.com>
2023-02-09 15:00:13 -0300

    parentNode and refactor code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 	// constants
 
-const braco = document.querySelector('#braco')
-const controleContador = document.querySelector(".controle-contador")
-const subtrair = document.querySelector('#subtrair')
-const somar = document.querySelector('#somar')
 const controle = document.querySelectorAll('.controle-ajuste')
 
 	// event forEach click
@@ -11,16 +7,18 @@ const controle = document.querySelectorAll('.controle-ajuste')
 controle.forEach((elemento) => {
 	elemento.addEventListener('click', (e) => {
 		e.preventDefault()
-		manipulaDados(e.target.textContent)
+		manipulaDados(e.target.textContent, e.target.parentNode)
 	})
 })
 
 	// function datas manipulation
 
-function manipulaDados(operacao){
+function manipulaDados(operacao, controle){
+	const peca = controle.querySelector('.controle-contador')
+
 	if(operacao === "-"){
-		braco.value = parseInt(braco.value) - 1
+		peca.value = parseInt(peca.value) - 1
 	} else {
-		braco.value = parseInt(braco.value) + 1
+		peca.value = parseInt(peca.value) + 1
 	}
 }
