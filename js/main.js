84bf48a0db5a4c529eaad66cf332121ccf15cb62
Weverton <wever-santos@hotmail.com>
2023-02-09 12:50:48 -0300

    function for datas manipulation and if-else

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
+	// constants
+
 const braco = document.querySelector('#braco')
 const subtrair = document.querySelector('#subtrair')
 const somar = document.querySelector('#somar')
+const controle = document.querySelectorAll('.controle-ajuste')
 
-somar.addEventListener('click', (evento) => {
-	evento.preventDefault()
-	braco.value = parseInt(braco.value) + 1
+	// event click
+
+somar.addEventListener('click', (e) => {
+	e.preventDefault()
+	manipulaDados("somar")
 })
 
-subtrair.addEventListener('click', (evento) => {
-	evento.preventDefault()
-	braco.value = parseInt(braco.value) - 1
+subtrair.addEventListener('click', (e) => {
+	e.preventDefault()
+	manipulaDados("subtrair")
 })
+
+	// function datas manipulation
+
+function manipulaDados(operacao){
+	if(operacao === "subtrair"){
+		braco.value = parseInt(braco.value) - 1
+	} else {
+		braco.value = parseInt(braco.value) + 1
+	}
+}
