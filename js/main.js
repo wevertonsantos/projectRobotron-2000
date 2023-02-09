454e4dccae61a134efe4c3c3255e4c8606abf090
Weverton <wever-santos@hotmail.com>
2023-02-09 14:17:57 -0300

    forEach and textContent

--- js/main.js
+++ js/main.js
@@ -1,26 +1,24 @@
 	// constants
 
 const braco = document.querySelector('#braco')
+const controleContador = document.querySelector(".controle-contador")
 const subtrair = document.querySelector('#subtrair')
 const somar = document.querySelector('#somar')
 const controle = document.querySelectorAll('.controle-ajuste')
 
-	// event click
+	// event forEach click
 
-somar.addEventListener('click', (e) => {
-	e.preventDefault()
-	manipulaDados("somar")
-})
-
-subtrair.addEventListener('click', (e) => {
-	e.preventDefault()
-	manipulaDados("subtrair")
+controle.forEach((elemento) => {
+	elemento.addEventListener('click', (e) => {
+		e.preventDefault()
+		manipulaDados(e.target.textContent)
+	})
 })
 
 	// function datas manipulation
 
 function manipulaDados(operacao){
-	if(operacao === "subtrair"){
+	if(operacao === "-"){
 		braco.value = parseInt(braco.value) - 1
 	} else {
 		braco.value = parseInt(braco.value) + 1
