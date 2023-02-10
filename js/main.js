6a6ad2de8e4b34be3f00eb9c013edeb701cf5554
Weverton <wever-santos@hotmail.com>
2023-02-10 18:11:46 -0300

    working with object

--- js/main.js
+++ js/main.js
@@ -1,20 +1,53 @@
 	// constants
 
-const controle = document.querySelectorAll('.controle-ajuste')
+const controle = document.querySelectorAll('[data-controle]')
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
 	// event forEach click
 
 controle.forEach((elemento) => {
 	elemento.addEventListener('click', (e) => {
 		e.preventDefault()
-		manipulaDados(e.target.textContent, e.target.parentNode)
+		manipulaDados(e.target.dataset.controle, e.target.parentNode)
 	})
 })
 
 	// function datas manipulation
 
 function manipulaDados(operacao, controle){
-	const peca = controle.querySelector('.controle-contador')
+	const peca = controle.querySelector('[data-contador]')
 
 	if(operacao === "-"){
 		peca.value = parseInt(peca.value) - 1
